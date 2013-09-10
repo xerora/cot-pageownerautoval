ee51692ad4d0a774a0947856e378e911c99e36c4
xerora <tyler@xaez.org>
2013-09-10 16:27:09 -0700

    minor update

--- pageownerautoval/pageownerautoval.page.edit.update.done.php
+++ pageownerautoval/pageownerautoval.page.edit.update.done.php
@@ -6,7 +6,7 @@ Hooks=page.edit.update.done
 ==================== */
 
 global $usr, $_original_page_state;
-if($rpage['page_state'] == 1 && $row_page['page_ownerid'] == $usr['id'] && $_original_page_state == 0 && $row['page_state'] == 0)
+if(!is_null($_original_page_state) && $rpage['page_state'] == 1 && $row_page['page_ownerid'] == $usr['id'] && $_original_page_state == 0 && $row['page_state'] == 0)
 {
 	$db->query("UPDATE $db_structure SET structure_count=structure_count+1 WHERE structure_code = ? AND structure_area = 'page'", $rpage['page_cat']);
 	$db->update($db_pages, array('page_state' => 0), $id);
